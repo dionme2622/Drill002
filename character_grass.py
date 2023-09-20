a2fd96978f40a417db535522fe9f8b78a7e7d7bf
hyunholee <dionme2622@gmail.com>
2023-09-20 21:13:40 +0900

    Drill02 제출 (2020182034 이현호)

--- character_grass.py
+++ character_grass.py
@@ -7,77 +7,53 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-
-
-def move_rect():
-    x = 450
-    y = 0
-    while(x< 780):
+def move_rect() :
+    for x in range(400, 782, 2) :
         clear_canvas_now()
         grass.draw_now(400,30)
-        character.draw_now(x,90)
-        x = x+2
+        character.draw_now(x, 90)
         delay(0.01)
-        
-    while(y < 450):
+    for y in range(90, 562, 2) :
         clear_canvas_now()
         grass.draw_now(400,30)
-        character.draw_now(780,90 + y)
-        y =  y+2
+        character.draw_now(780, y)
         delay(0.01)
-
-    while(x > 20) :
+    for x in range(780, 20, -2) :
         clear_canvas_now()
         grass.draw_now(400,30)
-        character.draw_now(x,540)
-        x = x-2
+        character.draw_now(x, 560)
         delay(0.01)
-
-    while(y > 90) :
+    for y in range(560, 92, -2) :
         clear_canvas_now()
         grass.draw_now(400,30)
-        character.draw_now(20,y)
-        y =  y-2
+        character.draw_now(20, y)
+        delay(0.01)
+    for x in range(20, 402, 2) :
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, 90)
         delay(0.01)
 
-    while(x < 400) :
+def move_round() :
+    for theta in range(270, 361, 1) :
+        x = 210 * math.cos(math.radians(theta))
+        y = 210 * math.sin(math.radians(theta))
         clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        x =  x+2
+        grass.draw_now(400, 30)
+        character.draw_now(x+400,y+300)
         delay(0.01)
- 
-def move_round():
-    theta = 270
-    x = 0
-    y = 0
-    while(theta < 360) :
-        x = (210 * math.cos(math.radians(theta)))
-        y = (210 * math.sin(math.radians(theta)))
+
+    for theta in range(0, 271, 1) :
+        x = 210 * math.cos(math.radians(theta))
+        y = 210 * math.sin(math.radians(theta))
         clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(400+x,300+y)
-        theta = theta + 2
+        grass.draw_now(400, 30)
+        character.draw_now(x+400,y+300)
         delay(0.01)
-    theta = 0
-    while(theta < 270) :
-        x = (210 * math.cos(math.radians(theta)))
-        y = (210 * math.sin(math.radians(theta)))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(400+x,300+y)
-        theta = theta + 2
-        delay(0.01)
-   
-for x in range(0,450,2):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)
-while(1) :     
+    
+while(1) :
     move_rect()
     move_round()
-    
 
 
 close_canvas()
